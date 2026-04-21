fa8804c1e4d78def4ad51bfd5e26b984dd8d1ca3
solanabettercall <solanabettercall@gmail.com>
2026-04-22 01:11:30 +0300

    wip

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install fish bat eza fzf fd-find git-delta -y
+curl -sS https://starship.rs/install.sh | sh
+
+fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher && fisher update'
+
+chsh -s $(which fish)
